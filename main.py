84fb39a2a01d2141b12b2027cd9c99d29a912be6
juliusott <juliusott@gmx.de>
2020-08-15 15:09:23 +0200

    start working on the agent

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from PySide2.QtWidgets import QApplication
 from GameWindow import GameWindow
+from GameEngine import GameEngine
 import sys
+from agent import DQN
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    game = GameWindow()
+    engine = GameEngine()
+    engine.start()
     app.exec_()
     sys.exit(app.exit())
